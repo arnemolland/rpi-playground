d919a1a4ee3610ee1423d2fc85d80829f80a9380
Arne Olai Lyngsmo Molland <arneolaim@gmail.com>
2018-07-08 18:45:46 +0200

    :confused: what

--- lcd/lcd-test.py
+++ lcd/lcd-test.py
@@ -29,7 +29,7 @@ lcd_rows = 2
 
 lcd  = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-while true:
+while True:
     if(IO.input(14) == True):
         IO.output(2, True)
         IO.output(3, False)
