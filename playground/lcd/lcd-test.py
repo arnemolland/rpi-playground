f98eff4580ccb945ef7fbb7ac75636c52cb76ef5
Arne Olai Lyngsmo Molland <arneolaim@gmail.com>
2018-07-08 18:23:47 +0200

    :construction: Changes

--- playground/lcd/lcd-test.py
+++ playground/lcd/lcd-test.py
@@ -1,5 +1,12 @@
 import Adafruit_CharLCD as LCD
+import RPi.GPIO as IO
 import time
+
+IO.setwarnings(False)
+IO.setmode(IO.BCM)
+IO.setup(2, IO.OUT) #GPIO 2 -> RED
+IO.setup(3, IO.OOUT) #GPIO 3 - GREEN
+IO.setup(14, IO.IN) #GPIO 14 - IR
 
 lcd_rs = 18
 lcd_en = 22
@@ -14,11 +21,22 @@ lcd_rows = 2
 
 lcd  = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-time.sleep(5.0)
+def loop:
+    time.sleep(5.0)
 
-i = 10
-while i > 0:
-    lcd.message(i)
-    n=i-1
+    for num in range(10, 0, 1):
+        lcd.message(num)
+        time.sleep(1.0)
 
-lcd.message('Takeoff!')
+    lcd.message('Takeoff!')
+
+while true:
+    if(IO.input(14) == True):
+        IO.output(2, True)
+        IO.output(3, False)
+
+    if(IO.input(14) == False):
+        IO.output(3, True)
+        IO.output(2, False)
+        
+    loop()
