95f533a17e59045b96894f350ec18ab5c2a85693
Arne Olai Lyngsmo Molland <arneolaim@gmail.com>
2018-07-08 15:20:40 +0200

    :confused: what?

--- playground/lcd/lcd-test.py
+++ playground/lcd/lcd-test.py
@@ -14,7 +14,7 @@ lcd_rows = 2
 
 lcd  = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-lcd.message('Hallo på do')
+lcd.message('Hallo paa do')
 
 time.sleep(5.0)
 
